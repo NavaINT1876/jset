1aa57c63685bb1e05537856da542dab65a97a477
Igor <dr.pill@ukr.net>
2016-07-09 11:37:51 +0300

    Markup is ready

--- view-main.php
+++ view-main.php
@@ -3,30 +3,47 @@
         <div class="col-md-2 col-lg-3"></div>
         <div class="col-xs-12 col-sm-12 col-md-8 col-lg-6">
             <div class="panel panel-default">
-                <div class="panel-heading">Leave your comment here</div>
+                <div class="panel-heading">
+                    <span class="author">WhoWrote</span>
+                    <span class="pull-right">Date: 12.12.2012 10:48 a.m.</span>
+                </div>
+                <div class="panel-body">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, cupiditate delectus dicta
+                        ex illo iste provident quas tempore. Accusamus aut distinctio ipsam voluptatem! Laborum magnam
+                        mollitia officia quis quisquam saepe.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-2 col-lg-3"></div>
+    </div>
+    <div class="row">
+        <div class="col-md-2 col-lg-3"></div>
+        <div class="col-xs-12 col-sm-12 col-md-8 col-lg-6">
+            <div class="panel panel-primary">
+                <div class="panel-heading">Leave your comment</div>
                 <div class="panel-body">
                     <form>
                         <div class="row">
                             <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
                                 <div class="form-group">
-                                    <label for="name">Your name</label>
-                                    <input type="text" name="name" placeholder="Name" class="form-control" id="name">
+                                    <label for="name">Your name (*)</label>
+                                    <input type="text" name="name" placeholder="Name" class="form-control" id="name" required>
                                 </div>
                             </div>
                             <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
                                 <div class="form-group">
-                                    <label for="email">Email address</label>
-                                    <input type="email" name="email" placeholder="Email" class="form-control" id="email">
+                                    <label for="email">Email address (*)</label>
+                                    <input type="email" name="email" placeholder="Email" class="form-control" id="email" required>
                                 </div>
                             </div>
                         </div>
                         <div class="form-group">
-                            <label for="message">Email address</label>
-                            <textarea placeholder="Your message" rows="4" class="form-control" id="message"></textarea>
+                            <label for="message">Your message (*)</label>
+                            <textarea placeholder="Your message" rows="4" class="form-control" id="message" required></textarea>
                         </div>
                         <div class="form-group">
-                            <label for="exampleInputFile">Logo image</label>
-                            <input type="file" id="exampleInputFile">
+                            <label for="logo">Logo image</label>
+                            <input type="file" id="logo">
                             <p class="help-block">Allowed types: JPG, GIF, PNG</p>
                         </div>
                         <button type="submit" class="btn btn-primary">Submit</button>
