bfbff7ecc3f2dd291f13f5ef9ab2300055111c63
Igor <dr.pill@ukr.net>
2016-07-09 23:37:12 +0300

    Errors reporting changed

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-error_reporting(E_ALL);
 include_once('db_conf.php');
 
 function __autoload($class_name) {
@@ -9,23 +8,3 @@ function __autoload($class_name) {
 $controller = new Controller();
 
 $route = $controller->getRoute();
-
-/**
- * TODO: rewrite these comments
- * In root directory configuration file is `conf.php`
- *
- * In root directory template(view) files are:
- * -- `header.php`. Header stands for top part of application.
- * -- `footer.php`. Footer stands for bottom part of application.
- * -- `sidebar.php`. Stands for sidebar. In sidebar there are no working links. It is created to feel closer to real project.
- * -- `404.php`. If route is not found.
- * -- `add-edit.php`. It stands for Adding/Editing news items
- * -- `list.php`. It stands for List Page and shows all news items with pagination.
- * -- `single-view.php`. Stands for View Page / comments page.
- *
- * In root directory Controller file is `Controller.php`. It works with Model and
- * passes data(variables) to view files.
- *
- * In root directory Model file is `Model.php`. It works with data from database and
- * passes it to Controller.
- */
